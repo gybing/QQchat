6847c412f4f2a1670a1af4ef3f642881ec38a18b
gyh <gyh@gyhtekiMacBook-Air.local>
2015-05-23 14:08:02 +0800

    Initial Commit

---
+++ 机器人/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  机器人
+//
+//  Created by gyh on 15/5/23.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 机器人/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  机器人
+//
+//  Created by gyh on 15/5/23.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
